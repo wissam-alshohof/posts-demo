5a782a5a16ec9bc3a35cfac556329aa105e44b02
wissam alshohof <wissam.alshohof@gmail.com>
2023-03-14 23:44:15 +0400

    home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './../assets/home.css';
+
+function Home(props) {
+    return (
+        <div className='home'>
+            Welcome to the Posts app
+        </div>
+    );
+}
+
+export default Home;
