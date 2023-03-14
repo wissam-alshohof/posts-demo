ddaa8fa73c7300c083ce11c9f5449109324833dc
wissam alshohof <wissam.alshohof@gmail.com>
2023-03-14 23:43:20 +0400

    adding spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./../assets/spinner.css";
+
+function Spinner(props) {
+    return (
+        <div className='spinner'>
+            
+        </div>
+    );
+}
+
+export default Spinner;
